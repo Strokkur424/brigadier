ee798bf7f74d21ebaee53d8f4d81d75d8087fe39
Strokkur24 <strokkur.24@gmail.com>
2025-10-26 17:57:54 +0100

    feat: add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.mojang.brigadier {
+  exports com.mojang.brigadier;
+  exports com.mojang.brigadier.arguments;
+  exports com.mojang.brigadier.builder;
+  exports com.mojang.brigadier.context;
+  exports com.mojang.brigadier.exceptions;
+  exports com.mojang.brigadier.suggestion;
+  exports com.mojang.brigadier.tree;
+}
